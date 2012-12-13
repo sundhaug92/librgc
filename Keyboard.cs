d12e2b60922a60229d9b99908b4f98e4d52eb795
Martin Sundhaug <martinsundhaug@gmail.com>
2012-12-13 02:14:32 +0100

    Added Keyboard.cs, implements keyboard up/down/tap

---
+++ Keyboard.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace librgc
+{
+    class Keyboard
+    {
+        [DllImport("user32.dll")]
+        static extern uint keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+        public static void KeyDown(int VK)
+        {
+            byte key = (byte)VK;
+            keybd_event((byte)key, 0, 0, 0);
+        }
+
+        public static void KeyUp(int VK)
+        {
+            byte key = (byte)VK;
+            keybd_event((byte)key, 0, 0x7F, 0);
+        }
+        public static void KeyTap(int VK)
+        {
+            KeyDown(VK);
+            KeyUp(VK);
+        }
+    }
+}
