9d51c8aa7661ce10ce9a7b2a68805e4d44c30717
Martin Sundhaug <martinsundhaug@gmail.com>
2012-12-13 01:28:58 +0100

    Made websocketlistener async

--- WebServer.cs
+++ WebServer.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
+using System.Threading.Tasks;
+using System.Net.WebSockets;
 
 namespace librgc
 {
@@ -22,47 +24,87 @@ namespace librgc
         };
 
         private HttpListener listener;
-
-        public void Start()
+        private void mouseHandler(WebSocket ws)
         {
-        HttpListener listener = new HttpListener();
-        listener.Prefixes.Add("http://*:23091/");
-        try
-        {
-            listener.Start();
-        }
-        catch (HttpListenerException hle)
-        {
-            var username = Environment.GetEnvironmentVariable("USERNAME");
-            var userdomain = Environment.GetEnvironmentVariable("USERDOMAIN");
-            if (hle.ErrorCode == 5)
+            byte[] buffer = new byte[4096];
+            var task = ws.ReceiveAsync(new ArraySegment<byte>(buffer), System.Threading.CancellationToken.None).ContinueWith((ReceiveResult) =>
             {
-                Console.WriteLine("You need to run the following command:");
-                Console.WriteLine("  netsh http add urlacl url={0} user={1}\\{2} listen=yes",
-                    "http://*:23091/", userdomain, username);
-                return;
-            }
-            else
-            {
-                throw;
-            }
+                string s = System.Text.Encoding.ASCII.GetString(buffer);
+                s = s.TrimEnd('\0', ' ');
+                foreach (string mouseCmd in s.Split(' ', '\0'))
+                {
+                    string cmd = mouseCmd.ToLower().Trim();
+                    if (mouseCmd == "reset/")
+                    {
+                        Mouse.SetCursorPos(500, 500);
+                    }
+                    if (mouseCmd.StartsWith("click/"))
+                    {
+                        switch (int.Parse(mouseCmd.Substring("click/".Length)))
+                        {
+                            case 0:
+                                Mouse.LeftClick();
+                                break;
+                            case 1:
+                                Mouse.RightClick();
+                                break;
+                        }
+                    }
+                    if (mouseCmd.StartsWith("position/"))
+                    {
+                        String[] stringArray = mouseCmd.Substring("position/".Length).Split('/');
+                        Mouse.SetCursorPos(int.Parse(stringArray[0]), int.Parse(stringArray[1]));
+                    }
+                }
+                mouseHandler(ws);
+            });
         }
-        Console.WriteLine("Listening...");
+        public async Task Start()
+        {
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://*:23091/");
+            try
+            {
+                listener.Start();
+            }
+            catch (HttpListenerException hle)
+            {
+                var username = Environment.GetEnvironmentVariable("USERNAME");
+                var userdomain = Environment.GetEnvironmentVariable("USERDOMAIN");
+                if (hle.ErrorCode == 5)
+                {
+                    Console.WriteLine("You need to run the following command:");
+                    Console.WriteLine("  netsh http add urlacl url={0} user={1}\\{2} listen=yes",
+                        "http://*:23091/", userdomain, username);
+                    return;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            Console.WriteLine("Listening...");
             try
             {
                 listener.Start();
                 while (true)
                 {
-                    listener.BeginGetContext((result) =>
+                    HttpListenerContext context = await listener.GetContextAsync();
+                    HttpListenerRequest request = context.Request;
+                    HttpListenerResponse response = context.Response;
+                    if (request.IsWebSocketRequest)
                     {
-                        HttpListener contextListener = (HttpListener)result.AsyncState;
-                        HttpListenerContext context = listener.EndGetContext(result);
-                        HttpListenerRequest request = context.Request;
-                        HttpListenerResponse response = context.Response;
-                        if (request.IsWebSocketRequest)
+                        context.AcceptWebSocketAsync(null).ContinueWith((AcceptWebSocketAsyncResult) =>
                         {
-                        }
-                        else{
+                            var wsContext = AcceptWebSocketAsyncResult.Result;
+                            if (request.RawUrl.ToLower() == "/mouse")
+                            {
+                                mouseHandler(wsContext.WebSocket);
+                            }
+                        });
+                    }
+                    else
+                    {
                         response.ProtocolVersion = new Version("1.0"); //Disable fancy http 1.1 features
 
                         string respPath = "www" + request.Url.AbsolutePath.ToLower();
@@ -104,8 +146,7 @@ namespace librgc
                             response.Headers[HttpResponseHeader.Connection] = "Close";
                             response.Close();
                         }
-                        }
-                    }, null);
+                    }
                     listenForNextRequest.WaitOne();
                 }
             }
