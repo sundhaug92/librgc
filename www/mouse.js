23d251b1c1818cce8920fa3f86f7044ebf2cd0c6
Martin Sundhaug <martinsundhaug@gmail.com>
2013-01-12 17:06:27 +0100

    Mouse is now an object

--- www/mouse.js
+++ www/mouse.js
@@ -9,3 +9,41 @@ function RightClick() {
 function MiddleClick() {
     mouseConnection.send("click/2");
 }
+var Mouse = {
+    Connection: new WebSocket("ws://192.168.1.14:23091/mouse"),
+    RequireConnectionUp: function (callback) {
+        if (this.Connection.readyState == this.Connection.OPEN) {
+            callback();
+        }
+        else {
+            this.Connection = new WebSocket("ws://192.168.1.14:23091/keyboard");
+            this.Connection.onopen(callback);
+        }
+    },
+    Send: function (string) {
+        this.RequireConnectionUp(function () {
+            this.Connection.send(string);
+        });
+    },
+    MiddleClick: function () {
+        this.Click(2);
+    },
+    RightClick: function () {
+        this.Click(1);
+    },
+    LeftClick:function(){
+        this.Click(0);
+    },
+    Click:function(key){
+        this.Send("click/" + key);
+    },
+    Up: function (key) {
+        this.Send("up/" + key);
+    },
+    Down: function (key) {
+        this.Send("down/" + key);
+    },
+    Reset: function () {
+        this.Send("reset/");
+    }
+};
