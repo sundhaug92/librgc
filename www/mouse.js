5be6455367f7699b01e70bed963bf57586006696
Martin Sundhaug <martinsundhaug@gmail.com>
2013-01-12 16:57:51 +0100

    Moved mouseConnection

--- www/mouse.js
+++ www/mouse.js
@@ -1,12 +1,11 @@
-﻿function LeftClick() {
+﻿var mouseConnection = new WebSocket("ws://192.168.1.14:23091/mouse");
+
+function LeftClick() {
     mouseConnection.send("click/0");
 }
 function RightClick() {
     mouseConnection.send("click/1");
-
 }
 function MiddleClick() {
     mouseConnection.send("click/2");
-
 }
-var mouseConnection = new WebSocket("ws://192.168.1.14:23091/mouse");
