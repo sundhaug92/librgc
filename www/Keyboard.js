e20e7e44ec6142e6c6c880a1f0b1a3aa0c7598f2
Martin Sundhaug <martinsundhaug@gmail.com>
2013-01-12 20:19:33 +0100

    Added bunch of VK definitions

--- www/Keyboard.js
+++ www/Keyboard.js
@@ -37,14 +37,80 @@ var Keyboard = {
         this.Send("down/" + key);
     },
     VK: {
+        BACKSPACE: '08',
+        TAB: '09',
+
+        ENTER: '0D',
+
+        SHIFT: '10',
+        CONTROL: '11',
+        MENU: '12',
+        PAUSE: '13',
+        CAPITAL: '14',
+        CAPSLOCK: '14',
+
+        ESCAPE: '1B',
+
+        SPACE: '20',
+        PRIOR: '21',
+        PAGE_UP: '21',
+        NEXT: '22',
+        PAGE_DOWN: '22',
+        END: '23',
+        HOME: '24',
+        LEFT: '25',
+        UP: '26',
+        RIGHT: '27',
+        DOWN: '28',
+
+        AT: '40',
+        A: '41',
+        B: '42',
+        C: '43',
+        D: '44',
+        E: '45',
+        F: '46',
+        G: '47',
+        H: '48',
+        I: '49',
+        J: '4A',
+        K: '4B',
+        L: '4C',
+        M: '4D',
+        N: '4E',
+        O: '4F',
+        P: '50',
+        Q: '51',
+        R: '52',
+        S: '53',
+        T: '54',
+        U: '55',
+        V: '56',
+        W: '57',
+        X: '58',
+        Y: '59',
+        Z: '5A',
+
+        ADD: '6B',
+        SEPARATOR: '6C',
+        SUBTRACT: '6D',
+        DECIMAL: '6E',
+        DIVIDE: '6F',
+
+        NUMLOCK: '90',
+        SCROLLOCK: '91',
+
         VOLUME_MUTE: 'AD',
         VOLUME_DOWN: 'AE',
         VOLUME_UP: 'AF',
         MEDIA_NEXT_TRACK: 'B0',
         MEDIA_PREV_TRACK: 'B1',
         MEDIA_STOP: 'B2',
-        MEDIA_PLAY_PAUSE: 'B3'
-    },
-
+        MEDIA_PLAY_PAUSE: 'B3',
+        LAUNCH_MAIL: 'B4',
+        LAUNCH_MEDIA_SELECT: 'B5',
+        LAUNCH_APP1: 'B6',
+        LAUNCH_APP2: 'B7'
+    }
 };
 
