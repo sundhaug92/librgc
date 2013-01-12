79a9b114976930ba7528cafb08133efddc70ae58
Martin Sundhaug <martinsundhaug@gmail.com>
2013-01-12 05:41:10 +0100

    Implemented mouse and keyboard javascript support

---
+++ www/Keyboard.js
@@ -0,0 +1,11 @@
+﻿function keyboardTap(VK){
+    keyboardConnection.send("tap/" + VK);
+}
+function keyboardUp(VK){
+    keyboardConnection.send("up/" + VK);
+}
+function keyboardDown(VK){
+    keyboardConnection.send("down/", VK);
+}
+
+var keyboardConnection = new WebSocket("ws://192.168.1.14:23091/keyboard");
---
+++ www/mouse.js
@@ -0,0 +1,12 @@
+﻿function LeftClick() {
+    mouseConnection.send("click/0");
+}
+function RightClick() {
+    mouseConnection.send("click/1");
+
+}
+function MiddleClick() {
+    mouseConnection.send("click/2");
+
+}
+var mouseConnection = new WebSocket("ws://192.168.1.14:23091/mouse");
