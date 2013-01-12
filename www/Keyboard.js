482a677efd7c7d8ab125815007fa6bee5f09bb9c
Martin Sundhaug <martinsundhaug@gmail.com>
2013-01-12 16:50:57 +0100

    Keyboard.RequireConnectionUp implemented

--- www/Keyboard.js
+++ www/Keyboard.js
@@ -13,6 +13,15 @@ function keyboardDown(VK) {
 
 var Keyboard = {
     Connection: new WebSocket("ws://192.168.1.14:23091/keyboard"),
+    RequireConnectionUp: function (callback) {
+        if (this.Connection.readyState == this.Connection.OPEN) {
+            callback();
+        }
+        else {
+            this.Connection = new WebSocket("ws://192.168.1.14:23091/keyboard");
+            this.Connection.onopen(callback);
+        }
+    },
     Tap: function (VK) {
         keyboardConnection.send("tap/" + VK);
     },
