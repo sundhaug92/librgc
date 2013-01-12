0b89a7f41d0e9cb795bfe011e28f90b459e3d1af
Martin Sundhaug <martinsundhaug@gmail.com>
2013-01-12 19:23:00 +0100

    C# Keyboard API now takes scan and ExtraInfo parameters

--- Keyboard.cs
+++ Keyboard.cs
@@ -8,21 +8,21 @@ namespace librgc
     {
         [DllImport("user32.dll")]
         static extern uint keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
-        public static void KeyDown(int VK)
+        public static void KeyDown(int VK, byte scan = 0, int ExtraInfo = 0)
         {
             byte key = (byte)VK;
-            keybd_event((byte)key, 0, 0, 0);
+            keybd_event((byte)key, scan, 0, ExtraInfo);
         }
 
-        public static void KeyUp(int VK)
+        public static void KeyUp(int VK, byte scan = 0, int ExtraInfo = 0)
         {
             byte key = (byte)VK;
-            keybd_event((byte)key, 0, 0x7F, 0);
+            keybd_event((byte)key, scan, 0x7F, ExtraInfo);
         }
-        public static void KeyTap(int VK)
+        public static void KeyTap(int VK, byte scan = 0, int ExtraInfo = 0)
         {
-            KeyDown(VK);
-            KeyUp(VK);
+            KeyDown(VK,scan, ExtraInfo);
+            KeyUp(VK, scan, ExtraInfo);
         }
     }
 }
