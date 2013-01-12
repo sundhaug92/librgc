e8db1937c41cfe1cebfe9052083bee56260b30ff
Martin Sundhaug <martinsundhaug@gmail.com>
2013-01-12 16:40:37 +0100

    Keyboard is now an object

--- www/Keyboard.js
+++ www/Keyboard.js
@@ -1,11 +1,24 @@
-﻿function keyboardTap(VK){
-    keyboardConnection.send("tap/" + VK);
+﻿var keyboardConnection = new WebSocket("ws://192.168.1.14:23091/keyboard");
+
+function keyboardTap(VK) {
+    Keyboard.Tap(VK);
 }
-function keyboardUp(VK){
-    keyboardConnection.send("up/" + VK);
+function keyboardUp(VK) {
+    Keyboard.Up(VK);
 }
-function keyboardDown(VK){
-    keyboardConnection.send("down/", VK);
+function keyboardDown(VK) {
+    Keyboard.Down(VK);
 }
 
-var keyboardConnection = new WebSocket("ws://192.168.1.14:23091/keyboard");
+var Keyboard = {
+    Tap: function (VK) {
+        keyboardConnection.send("tap/" + VK);
+    },
+    Up: function (VK) {
+        keyboardConnection.send("up/" + VK);
+    },
+    Down: function (VK) {
+        keyboardConnection.send("down/" + VK);
+    }
+};
+
