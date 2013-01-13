ae9dbc612f631b013c429c15e23e77e84b7667a5
Martin Sundhaug <martinsundhaug@gmail.com>
2013-01-13 04:26:14 +0100

    Implemented Age of Empires 2 API

---
+++ www/AOE2/AOE2.js
@@ -0,0 +1,73 @@
+﻿var Unit = {
+    AttackGround: function () {
+        Keyboard.Tap(Keyboard.VK.T);
+    },
+    Convert: function () {
+        Keyboard.Tap(Keyboard.VK.C);
+    },
+    Delete: function () {
+        Keyboard.Tap(Keyboard.VK.Delete);
+    },
+    EconomicBuildings: function () {
+        Keyboard.Tap(Keyboard.VK.B);
+    },
+    Garrison: function () {
+        Keyboard.Tap(Keyboard.VK.Garrison);
+    },
+    Heal: function () {
+        Keyboard.Tap(Keyboard.VK.E);
+    },
+    MilitaryBuildings: function () {
+        Keyboard.Tap(Keyboard.VK.V);
+    },
+    Pack: function () {
+        Keyboard.Tap(Keyboard.VK.P);
+    },
+    Repair: function () {
+        Keyboard.Tap(Keyboard.VK.R);
+    },
+    SetGatherPoint: function () {
+        Keyboard.Tap(Keyboard.VK.I);
+    },
+    Stop: function () {
+        Keyboard.Tap(Keyboard.VK.S);
+    },
+    Unload: function () {
+        Keyboard.Tap(Keyboard.VK.L);
+    },
+    Unpack: function () {
+        Keyboard.Tap(Keyboard.VK.U);
+    }
+};
+
+var Scroll = {
+    Down: function () {
+        Keyboard.Tap(Keyboard.VK.Down);
+    },
+    Left: function () {
+        Keyboard.Tap(Keyboard.VK.Left);
+    },
+    Right: function () {
+        Keyboard.Tap(Keyboard.VK.Right);
+    },
+    Up: function () {
+        Keyboard.Tap(Keyboard.VK.Up);
+    }
+};
+
+var Castle = {
+    Trebuchet: function () {
+        Keyboard.Tap(Keyboard.VK.R);
+    },
+    UniqueUnit: function () {
+        Keyboard.Tap(Keyboard.VK.T);
+    },
+    Trebuchet: function () {
+        Keyboard.Tap(Keyboard.VK.P);
+    }
+};
+var Mill = {
+    ReseedFarm: function () {
+        Keyboard.Tap(Keyboard.VK.F);
+    }
+}
