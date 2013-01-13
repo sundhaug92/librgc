ede30fd56ad1f53ccf74e3308385f08a8fe83b87
Martin Sundhaug <martinsundhaug@gmail.com>
2013-01-13 03:47:43 +0100

    Added App.js

---
+++ www/App.js
@@ -0,0 +1,11 @@
+﻿var App = {
+    Launch: function (name) {
+        Keyboard.Tap(Keyboard.VK.LWIN);
+        Keyboard.Type(name + '\n');
+    },
+    Exit: function(){
+        Keyboard.Down(Keyboard.VK.MENU);
+        Keyboard.Tap(Keyboard.VK.F4)
+        Keyboard.Up(Keyboard.VK.MENU);
+    }
+};
