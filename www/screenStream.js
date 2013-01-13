e5d395ebd6e0bf8d369ba1a6105c3c2afda1e7fa
Martin Sundhaug <martinsundhaug@gmail.com>
2013-01-13 22:14:08 +0100

    Attempted  fix to put image on canvas

--- www/screenStream.js
+++ www/screenStream.js
@@ -12,16 +12,17 @@
     },
     Init: function () {
         screenStream.Connection.binaryType = "arraybuffer";
-        screenStream.Connection.onmessage = function () {
+        screenStream.Connection.onmessage = function (messageEvent) {
             var destinationCanvas = document.getElementById('screenCanvas');
             var destinationContext = destinationCanvas.getContext('2d');
             var image = new Image();
             image.onload = function () {
                 destinationContext.clearRect(0, 0,
-                   destinationCanvas.width, destinationCanvas.height);
+                destinationCanvas.width, destinationCanvas.height);
                 destinationContext.drawImage(image, 0, 0);
+                debugger;
             }
-            image.src = URL.createObjectURL(messageEvent.data);
+            image.src = URL.createObjectURL(new Blob(messageEvent.data));
         };
     }
 };
