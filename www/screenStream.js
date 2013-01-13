1e95c76892809f8ed38a941a0c68f111372f61ed
Martin Sundhaug <martinsundhaug@gmail.com>
2013-01-13 19:36:10 +0100

    Added screenStream

---
+++ www/screenStream.js
@@ -0,0 +1,27 @@
+﻿var screenStream = {
+    Connection: new WebSocket("ws://" + window.location.host + "/screenstream"),
+    RequireConnectionUp: function (callback) {
+        if (this.Connection.readyState == this.Connection.OPEN) {
+            callback();
+        }
+        else {
+            screenStream.Connection = new WebSocket(screenStream.Connection.url);
+            init();
+            screenStream.Connection.onopen = callback;
+        }
+    },
+    Init: function () {
+        screenStream.Connection.binaryType = "arraybuffer";
+        screenStream.Connection.onmessage = function () {
+            var destinationCanvas = document.getElementById('imageCanvas');
+            var destinationContext = destinationCanvas.getContext('2d');
+            var image = new Image();
+            image.onload = function () {
+                destinationContext.clearRect(0, 0,
+                   destinationCanvas.width, destinationCanvas.height);
+                destinationContext.drawImage(image, 0, 0);
+            }
+            image.src = URL.createObjectURL(messageEvent.data);
+        };
+    }
+};
