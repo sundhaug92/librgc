659d5d0ba6fdd4b4734ed8466922ef5ebd644e13
Martin Sundhaug <martinsundhaug@gmail.com>
2013-04-27 01:01:07 +0200

    Updated copyright and version info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("librgc")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2012.1002.0.16")]
-[assembly: AssemblyFileVersion("2012.1002.0.16")]
+[assembly: AssemblyVersion("2013.0.*")]
+//[assembly: AssemblyFileVersion("2012.1002.0.16")]
